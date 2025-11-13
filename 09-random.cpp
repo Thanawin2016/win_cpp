660bd8ebe47622f6454114bd8b190ac41c6c55b0
Thanawin2016 <thanawin2016.t@hotmail.com>
2025-11-13 19:15:30 +0700

    feat: update random

--- 09-random.cpp
+++ 09-random.cpp
@@ -9,13 +9,19 @@ int main() {
 
     // Generate a random integer
     
-    int rawNumber = rand(); 
+    int rawNumber1 = rand();
+    int rawNumber2 = rand(); 
 
-    cout << "RawNumber = " << rawNumber << "\n";
+    cout << "RawNumber1 = " << rawNumber1 << "\n";
+    cout << "RawNumber2 = " << rawNumber2 << "\n";
 
-    int random = rawNumber % 6;
+    int random1 = (rawNumber1 % 6) + 1;
+    int random2 = (rawNumber2 % 6) + 1;
 
-    cout << "Random = " << random << "\n";
+    cout << "Random1 = " << random1 << "\n";
+    cout << "Random2 = " << random2 << "\n";
+
+    cout << "TOTAL = " << random1 << " + " << random2 << " = "  << random1 +  random2  ;
 
     return 0;
 }
