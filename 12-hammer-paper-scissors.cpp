fd8e9d4e742910e1e5ac6f39f3458ecb83cd692a
Thanawin2016 <thanawin2016.t@hotmail.com>
2025-11-29 10:45:18 +0700

    feat: add new hammer paper scissors game

--- 12-hammer-paper-scissors.cpp
+++ 12-hammer-paper-scissors.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 int main() {
+    int q;
+
     // Get a different random number each time the program runs
     srand(time(0));
 
@@ -11,13 +13,36 @@ int main() {
     int rawNumber1 = rand();
     int random1 = (rawNumber1 % 3);
 
-    if (random1 == 0) {
-        cout << "AI:= hammer";
-    } else if (random1 == 1) {
-        cout << "AI:= paper";
-    } else {
-        cout << "AI:= scissors";
-    }
+        cout << "0 = :hammer: 1 = :paper: 2 = :scissors:\n";
+        cout << ":Input your operation:";
+        cin >> q;
+
+                if (q == 0) {
+                cout << "your:= hammer\n";
+            } else if (q == 1) {
+                cout << "your:= paper\n";
+            } else if (q == 2) {
+                cout << "your:= scissors\n";
+            } else {
+                cout << "ERROR_404\n"; 
+            }
+        
+            if (random1 == 0) {
+                cout << "AI:= hammer";
+            } else if (random1 == 1) {
+                cout << "AI:= paper";
+            } else if (random1 == 2) {
+                cout << "AI:= scissors";
+            }
+
+            if (q == random1)
+            {
+                cout << "no one is win";
+            } else if (q < random1 && random1 < 2) {
+                cout << "AI is win";
+            }
+            
+
 
     return 0;
 }
