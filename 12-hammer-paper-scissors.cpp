bbcb5057417b4fa6202f63e88240acee61a9ad83
Thanawin2016 <thanawin2016.t@hotmail.com>
2025-12-23 20:04:48 +0700

    feat: look my new hammer paper scissors game

--- 12-hammer-paper-scissors.cpp
+++ 12-hammer-paper-scissors.cpp
@@ -13,37 +13,38 @@ int main() {
     int rawNumber1 = rand();
     int random1 = (rawNumber1 % 3);
 
-        cout << "0 = :hammer: 1 = :paper: 2 = :scissors:\n";
-        cout << ":Input your operation:";
-        cin >> q;
+    cout << "0 = :hammer: 1 = :paper: 2 = :scissors:\n";
+    cout << ":Input your operation:";
+    cin >> q;
 
-                if (q == 0) {
-                cout << "your:= hammer\n";
-            } else if (q == 1) {
-                cout << "your:= paper\n";
-            } else if (q == 2) {
-                cout << "your:= scissors\n";
-            } else {
-                cout << "ERROR_404\n"; 
-            }
-        
-            if (random1 == 0) {
-                cout << "AI:= hammer\n";
-            } else if (random1 == 1) {
-                cout << "AI:= paper\n";
-            } else if (random1 == 2) {
-                cout << "AI:= scissors\n";
-            }
+    // Show your operation
+    if (q == 0) {
+        cout << "your:= hammer\n";
+    } else if (q == 1) {
+        cout << "your:= paper\n";
+    } else if (q == 2) {
+        cout << "your:= scissors\n";
+    } else {
+        cout << "ERROR_404\n"; 
+    }
 
-            
-             if (q == random1) {
-                cout << "No one is win";
-            } else if (q == 0 && random1 == 1 || q == 1 && random1 == 2 || q == 2 && random1 == 0) {
-                cout << "AI is win";
-            } else {
-                cout << "you is win";
-            }
+    // Show AI operation
+    if (random1 == 0) {
+        cout << "AI:= hammer\n";
+    } else if (random1 == 1) {
+        cout << "AI:= paper\n";
+    } else if (random1 == 2) {
+        cout << "AI:= scissors\n";
+    }
 
+    // Check winner
+    if (q == random1) {
+        cout << "No one is win";
+    } else if (q == 0 && random1 == 1 || q == 1 && random1 == 2 || q == 2 && random1 == 0) {
+        cout << "AI is win";
+    } else {
+        cout << "you is win";
+    }
 
     return 0;
 }
