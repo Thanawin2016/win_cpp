6cd088146130812280de2cb62b387c921587e6d3
Thanawin2016 <thanawin2016.t@hotmail.com>
2025-12-24 19:42:23 +0700

    feat: look my new feature player score

--- 12-hammer-paper-scissors.cpp
+++ 12-hammer-paper-scissors.cpp
@@ -5,46 +5,59 @@ using namespace std;
 
 int main() {
     int q;
+    char ch;
+    ch = 'y';
+    int w;
+    w = 0;
 
-    // Get a different random number each time the program runs
-    srand(time(0));
+    while (ch == 'y') {
+        // Get a different random number each time the program runs
+        srand(time(0));
 
-    // Generate a random integer
-    int rawNumber1 = rand();
-    int random1 = (rawNumber1 % 3);
+        // Generate a random integer
+        int rawNumber1 = rand();
+        int random1 = (rawNumber1 % 3);
 
-    cout << "0 = :hammer: 1 = :paper: 2 = :scissors:\n";
-    cout << ":Input your operation:";
-    cin >> q;
+        cout << "0 = :hammer: 1 = :paper: 2 = :scissors:\n";
+        cout << ":Input your operation:";
+        cin >> q;
 
-    // Show your operation
-    if (q == 0) {
-        cout << "your:= hammer\n";
-    } else if (q == 1) {
-        cout << "your:= paper\n";
-    } else if (q == 2) {
-        cout << "your:= scissors\n";
-    } else {
-        cout << "ERROR_404\n"; 
+        // Show your operation
+        if (q == 0) {
+            cout << "your:= hammer\n";
+        } else if (q == 1) {
+            cout << "your:= paper\n";
+        } else if (q == 2) {
+            cout << "your:= scissors\n";
+        } else {
+            cout << "ERROR_404\n"; 
+        }
+
+        // Show AI operation
+        if (random1 == 0) {
+            cout << "AI:= hammer\n";
+        } else if (random1 == 1) {
+            cout << "AI:= paper\n";
+        } else if (random1 == 2) {
+            cout << "AI:= scissors\n";
+        }
+
+        // Check winner
+        if (q == random1) {
+            cout << "No one is win\n";
+        } else if (q == 0 && random1 == 1 || q == 1 && random1 == 2 || q == 2 && random1 == 0) {
+            cout << "AI is win\n";
+        } else {
+            cout << "you is win\n";
+            w += 1;
+        }
+
+        cout << "Are you continue? (y or n):";
+		cin >> ch;
+
     }
 
-    // Show AI operation
-    if (random1 == 0) {
-        cout << "AI:= hammer\n";
-    } else if (random1 == 1) {
-        cout << "AI:= paper\n";
-    } else if (random1 == 2) {
-        cout << "AI:= scissors\n";
-    }
-
-    // Check winner
-    if (q == random1) {
-        cout << "No one is win";
-    } else if (q == 0 && random1 == 1 || q == 1 && random1 == 2 || q == 2 && random1 == 0) {
-        cout << "AI is win";
-    } else {
-        cout << "you is win";
-    }
+    cout << "Your score is " << w ;
 
     return 0;
 }
